ddc55464a32e0a3f5f8d1a20c930cee093fd19c8
frostwing98 <498924217@qq.com>
2025-03-09 23:21:22 -0400

    'update'

--- decrypt.sh
+++ decrypt.sh
@@ -2,5 +2,6 @@
 #go run wxapkg_decrypt.go -wxid packages/$1 -in packages/$1.wxapkg -out packages/$1-o.wxapkg
 wxid=$1
 echo ${wxid:0:4} ${wxid:4:2}
-cp /storage/miniapp/wechat/packages/${wxid:0:4}/${wxid:4:2}/$1.wxapkg ./output/
+# cp /storage/miniapp/wechat/packages/${wxid:0:4}/${wxid:4:2}/$1.wxapkg ./output/
+cp ./miniapp_packages/$1.wxapkg ./output/
 node wuWxapkg.js output/$1.wxapkg
